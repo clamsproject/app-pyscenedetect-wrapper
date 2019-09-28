cb077057c30c137cf33989daf8be3ccffb7c0857
Kelley Lynch <kelleylynch@gmail.com>
2019-09-27 22:01:11 -0400

    fixing annotation type

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 import sys
-import json
 from scene_detect import SceneDetection
 
-bd = SceneDetection()
+tool = SceneDetection()
 a = open(sys.argv[1])
 b = a.read()
-c = bd.annotate(b)
-for i in c.views:
-    a = i.__dict__
-    print (a)
-    c = a.get("contains")
-    bd = a.get("annotations")
-    for d in bd:
-        print (d.__dict__)
+c = tool.annotate(b)
+with open (sys.argv[2], "w") as out:
+    out.write(c)
+# for i in c.views:
+#     a = i.__dict__
+#     print (a)
+#     c = a.get("contains")
+#     bd = a.get("annotations")
+#     for d in bd:
+#         print (d.__dict__)
